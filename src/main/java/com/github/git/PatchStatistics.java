4ea41076a0d13b25fc224abe83f9de173f2fc7b4
Raquel Pau <raquelpau@gmail.com>
2017-04-19 08:58:55 +0200

    patch modifications

---
+++ src/main/java/com/github/git/PatchStatistics.java
@@ -0,0 +1,30 @@
+package com.github.git;
+
+
+public class PatchStatistics {
+
+    private int additions;
+
+    private int modifications;
+
+    private int deletions;
+
+    public PatchStatistics(int additions, int modifications, int deletions) {
+        this.additions = additions;
+        this.modifications = modifications;
+        this.deletions = deletions;
+    }
+
+    public int getAdditions() {
+        return additions;
+    }
+
+    public int getModifications() {
+        return modifications;
+    }
+
+    public int getDeletions() {
+        return deletions;
+    }
+
+}
