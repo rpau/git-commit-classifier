180b8d83e4cc86480cfd643e28e1b7a7b39928bf
Raquel Pau <raquel.pau@schibsted.com>
2017-11-23 16:08:30 +0100

    initial controller

--- classifier-service/src/main/java/org/github/classifier/server/HelloController.java
+++ classifier-service/src/main/java/org/github/classifier/server/HelloController.java
@@ -1,14 +1,23 @@
 package org.github.classifier.server;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-  @RequestMapping("/")
+  @RequestMapping(value = "/", method = RequestMethod.GET)
   public String index() {
     return "Greetings from Spring Boot!";
   }
 
+
+  @RequestMapping(value = "/", method = RequestMethod.POST)
+  public void consume(@RequestBody JsonNode node) {
+
+    System.out.print("RECEIVED!!!");
+  }
 }
