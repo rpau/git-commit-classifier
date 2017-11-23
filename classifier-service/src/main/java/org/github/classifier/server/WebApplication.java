9bf915bedeea5bce3df8de7ec525a04c0f27324c
Raquel Pau <raquel.pau@schibsted.com>
2017-11-23 15:45:43 +0100

    spring boot and idea files deleted

---
+++ classifier-service/src/main/java/org/github/classifier/server/WebApplication.java
@@ -0,0 +1,15 @@
+package org.github.classifier.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WebApplication {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(WebApplication.class, args);
+  }
+
+}
